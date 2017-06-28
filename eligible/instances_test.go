a21f8d63dae242bbf5f83bfde6da7f2c4a8ff051
Lorin Hochstein <lhochstein@netflix.com>
2017-06-28 16:47:39 -0700

    checkpoint: WIP

--- eligible/instances_test.go
+++ eligible/instances_test.go
@@ -20,88 +20,91 @@ import (
 	"github.com/Netflix/chaosmonkey"
 	D "github.com/Netflix/chaosmonkey/deploy"
 	"github.com/Netflix/chaosmonkey/grp"
+	"github.com/Netflix/chaosmonkey/mock"
 )
 
-// mockApp returns a deploy.App object mock for testing
-func mockApp() *D.App {
+// mockDeployment returns a deploy.Deployment object mock for testing
+func mockDep() D.Deployment {
 	usEast1 := D.RegionName("us-east-1")
 	usWest2 := D.RegionName("us-west-2")
-	return D.NewApp("mock", D.AppMap{
-		D.AccountName("prod"): {
-			CloudProvider: "aws",
-			Clusters: D.ClusterMap{
-				D.ClusterName("mock-prod-a"): {
-					usEast1: {
-						D.ASGName("mock-prod-a-v123"): []D.InstanceID{"i-4a003cd0"},
-					},
-					usWest2: {
-						D.ASGName("mock-prod-a-v111"): []D.InstanceID{"i-efdc42dc"},
-					},
-				},
-				D.ClusterName("mock-prod-b"): {
-					usEast1: {
-						D.ASGName("mock-prod-b-v002"): []D.InstanceID{"i-115ccc27"},
-					},
-					usWest2: {
-						D.ASGName("mock-prod-b-v001"): []D.InstanceID{"i-7881287e"},
-					},
-				},
-				D.ClusterName("mock-staging-a"): {
-					usEast1: {
-						D.ASGName("mock-staging-a-v123"): []D.InstanceID{"i-ff8e7e4b"},
-					},
-					usWest2: {
-						D.ASGName("mock-staging-a-v111"): []D.InstanceID{"i-6eed18a4"},
-					},
-				},
-				D.ClusterName("mock-staging-b"): {
-					usEast1: {
-						D.ASGName("mock-staging-b-v002"): []D.InstanceID{"i-13770e40"},
-					},
-					usWest2: {
-						D.ASGName("mock-staging-b-v001"): []D.InstanceID{"i-afb7595e"},
-					},
-				},
-			},
-		},
-		D.AccountName("test"): {
-			CloudProvider: "aws",
-			Clusters: D.ClusterMap{
-				D.ClusterName("mock-test-a"): {
-					usEast1: {
-						D.ASGName("mock-test-a-v123"): []D.InstanceID{"i-23b61f89"},
-					},
-					usWest2: {
-						D.ASGName("mock-test-a-v111"): []D.InstanceID{"i-fe7a0827"},
+	return mock.NewDeployment(
+		map[string]D.AppMap{
+			"mock": {
+				D.AccountName("prod"): {
+					CloudProvider: "aws",
+					Clusters: D.ClusterMap{
+						D.ClusterName("mock-prod-a"): {
+							usEast1: {
+								D.ASGName("mock-prod-a-v123"): []D.InstanceID{"i-4a003cd0"},
+							},
+							usWest2: {
+								D.ASGName("mock-prod-a-v111"): []D.InstanceID{"i-efdc42dc"},
+							},
+						},
+						D.ClusterName("mock-prod-b"): {
+							usEast1: {
+								D.ASGName("mock-prod-b-v002"): []D.InstanceID{"i-115ccc27"},
+							},
+							usWest2: {
+								D.ASGName("mock-prod-b-v001"): []D.InstanceID{"i-7881287e"},
+							},
+						},
+						D.ClusterName("mock-staging-a"): {
+							usEast1: {
+								D.ASGName("mock-staging-a-v123"): []D.InstanceID{"i-ff8e7e4b"},
+							},
+							usWest2: {
+								D.ASGName("mock-staging-a-v111"): []D.InstanceID{"i-6eed18a4"},
+							},
+						},
+						D.ClusterName("mock-staging-b"): {
+							usEast1: {
+								D.ASGName("mock-staging-b-v002"): []D.InstanceID{"i-13770e40"},
+							},
+							usWest2: {
+								D.ASGName("mock-staging-b-v001"): []D.InstanceID{"i-afb7595e"},
+							},
+						},
 					},
 				},
-				D.ClusterName("mock-test-b"): {
-					usEast1: {
-						D.ASGName("mock-test-b-v002"): []D.InstanceID{"i-f581d5c3"},
-					},
-					usWest2: {
-						D.ASGName("mock-test-b-v001"): []D.InstanceID{"i-986e988a"},
+				D.AccountName("test"): {
+					CloudProvider: "aws",
+					Clusters: D.ClusterMap{
+						D.ClusterName("mock-test-a"): {
+							usEast1: {
+								D.ASGName("mock-test-a-v123"): []D.InstanceID{"i-23b61f89"},
+							},
+							usWest2: {
+								D.ASGName("mock-test-a-v111"): []D.InstanceID{"i-fe7a0827"},
+							},
+						},
+						D.ClusterName("mock-test-b"): {
+							usEast1: {
+								D.ASGName("mock-test-b-v002"): []D.InstanceID{"i-f581d5c3"},
+							},
+							usWest2: {
+								D.ASGName("mock-test-b-v001"): []D.InstanceID{"i-986e988a"},
+							},
+						},
+						D.ClusterName("mock-beta-a"): {
+							usEast1: {
+								D.ASGName("mock-beta-a-v123"): []D.InstanceID{"i-4b359d5d"},
+							},
+							usWest2: {
+								D.ASGName("mock-beta-a-v111"): []D.InstanceID{"i-e751bdd2"},
+							},
+						},
+						D.ClusterName("mock-beta-b"): {
+							usEast1: {
+								D.ASGName("mock-beta-b-v002"): []D.InstanceID{"i-e5eeba5e"},
+							},
+							usWest2: {
+								D.ASGName("mock-beta-b-v001"): []D.InstanceID{"i-76013ffb"},
+							},
+						},
 					},
 				},
-				D.ClusterName("mock-beta-a"): {
-					usEast1: {
-						D.ASGName("mock-beta-a-v123"): []D.InstanceID{"i-4b359d5d"},
-					},
-					usWest2: {
-						D.ASGName("mock-beta-a-v111"): []D.InstanceID{"i-e751bdd2"},
-					},
-				},
-				D.ClusterName("mock-beta-b"): {
-					usEast1: {
-						D.ASGName("mock-beta-b-v002"): []D.InstanceID{"i-e5eeba5e"},
-					},
-					usWest2: {
-						D.ASGName("mock-beta-b-v001"): []D.InstanceID{"i-76013ffb"},
-					},
-				},
-			},
-		},
-	})
+			}})
 }
 
 func testConfig(grouping chaosmonkey.Group) chaosmonkey.AppConfig {
@@ -115,11 +118,14 @@ func testConfig(grouping chaosmonkey.Group) chaosmonkey.AppConfig {
 }
 
 func TestGetEligibleInstances(t *testing.T) {
-	app := mockApp()
+	dep := mockDep()
 	group := grp.New("mock", "prod", "us-east-1", "", "mock-prod-a")
 	cfg := testConfig(chaosmonkey.Cluster)
 
-	instances := Instances(group, cfg, app)
+	instances, err := Instances(group, cfg, dep)
+	if err != nil {
+		t.Fatal(err)
+	}
 	got, want := len(instances), 1
 	if got != want {
 		t.Fatalf("len(eligibleInstances(group, cfg, app))=%v, want %v", got, want)
@@ -131,12 +137,15 @@ func TestGetEligibleInstances(t *testing.T) {
 }
 
 func TestDisabled(t *testing.T) {
-	app := mockApp()
+	dep := mockDep()
 	group := grp.New("mock", "prod", "us-east-1", "", "mock-prod-a")
 	cfg := testConfig(chaosmonkey.Cluster)
 	cfg.Enabled = false
 
-	instances := EligibleInstances(group, cfg, app)
+	instances, err := Instances(group, cfg, dep)
+	if err != nil {
+		t.Fatal(err)
+	}
 	got, want := len(instances), 0
 	if got != want {
 		t.Fatalf("len(eligibleInstances(group, cfg, app))=%v, want %v", got, want)
@@ -144,11 +153,14 @@ func TestDisabled(t *testing.T) {
 }
 
 func TestSimpleException(t *testing.T) {
-	app := mockApp()
+	dep := mockDep()
 	group := grp.New("mock", "prod", "us-east-1", "", "mock-prod-a")
 	cfg := testConfig(chaosmonkey.Cluster)
 	cfg.Exceptions = []chaosmonkey.Exception{{Account: "prod", Stack: "prod", Detail: "a", Region: "us-east-1"}}
-	instances := EligibleInstances(group, cfg, app)
+	instances, err := Instances(group, cfg, dep)
+	if err != nil {
+		t.Fatal(err)
+	}
 	got, want := len(instances), 0
 	if got != want {
 		t.Fatalf("len(eligibleInstances(group, cfg, app))=%v, want %v", got, want)
@@ -156,7 +168,7 @@ func TestSimpleException(t *testing.T) {
 }
 
 func TestMultipleExceptions(t *testing.T) {
-	app := abcloudMockApp()
+	app := abcloudMockDep()
 	// Group across everything in prod
 	group := grp.New("abcloud", "prod", "", "", "")
 	cfg := testConfig(chaosmonkey.Cluster)
@@ -166,7 +178,10 @@ func TestMultipleExceptions(t *testing.T) {
 		{Account: "prod", Stack: "", Detail: "", Region: "us-west-2"},
 	}
 
-	instances := EligibleInstances(group, cfg, app)
+	instances, err := Instances(group, cfg, app)
+	if err != nil {
+		t.Fatal(err)
+	}
 	got, want := len(instances), 6
 	if got != want {
 		t.Fatalf("len(eligibleInstances(group, cfg, app))=%v, want %v", got, want)
@@ -180,50 +195,53 @@ func TestMultipleExceptions(t *testing.T) {
 	}
 }
 
-// mockApp based on actual structure of abcloud
-func abcloudMockApp() *D.App {
+// mockDep based on actual structure of abcloud
+func abcloudMockDep() D.Deployment {
 	usEast1 := D.RegionName("us-east-1")
 	usWest2 := D.RegionName("us-west-2")
 	euWest1 := D.RegionName("eu-west-1")
-	m := D.AppMap{
-		D.AccountName("prod"): {
-			CloudProvider: "aws",
-			Clusters: D.ClusterMap{
-				D.ClusterName("abcloud"): {
-					usEast1: {
-						D.ASGName("abcloud-v123"): []D.InstanceID{"i-7921a2f8"},
-					},
-					usWest2: {
-						D.ASGName("abcloud-v123"): []D.InstanceID{"i-8a1bd7ac"},
-					},
-					euWest1: {
-						D.ASGName("abcloud-v123"): []D.InstanceID{"i-87a90e92"},
-					},
-				},
-				D.ClusterName("abcloud-batch"): {
-					usEast1: {
-						D.ASGName("abcloud-batch-v123"): []D.InstanceID{"i-2c25ab60"},
-					},
-					usWest2: {
-						D.ASGName("abcloud-batch-v123"): []D.InstanceID{"i-3bc40bdb"},
-					},
-					euWest1: {
-						D.ASGName("abcloud-batch-v123"): []D.InstanceID{"i-2910a0e4"},
-					},
-				},
-				D.ClusterName("abcloud-ecom"): {
-					usEast1: {
-						D.ASGName("abcloud-ecom-v123"): []D.InstanceID{"i-ab9a4f10"},
-					},
-					usWest2: {
-						D.ASGName("abcloud-ecom-v123"): []D.InstanceID{"i-b28a69c8"},
-					},
-					euWest1: {
-						D.ASGName("abcloud-ecom-v123"): []D.InstanceID{"i-4fa09365"},
+	return mock.NewDeployment(
+		map[string]D.AppMap{
+			"abcloud": {
+				D.AccountName("prod"): {
+					CloudProvider: "aws",
+					Clusters: D.ClusterMap{
+						D.ClusterName("abcloud"): {
+							usEast1: {
+								D.ASGName("abcloud-v123"): []D.InstanceID{"i-7921a2f8"},
+							},
+							usWest2: {
+								D.ASGName("abcloud-v123"): []D.InstanceID{"i-8a1bd7ac"},
+							},
+							euWest1: {
+								D.ASGName("abcloud-v123"): []D.InstanceID{"i-87a90e92"},
+							},
+						},
+						D.ClusterName("abcloud-batch"): {
+							usEast1: {
+								D.ASGName("abcloud-batch-v123"): []D.InstanceID{"i-2c25ab60"},
+							},
+							usWest2: {
+								D.ASGName("abcloud-batch-v123"): []D.InstanceID{"i-3bc40bdb"},
+							},
+							euWest1: {
+								D.ASGName("abcloud-batch-v123"): []D.InstanceID{"i-2910a0e4"},
+							},
+						},
+						D.ClusterName("abcloud-ecom"): {
+							usEast1: {
+								D.ASGName("abcloud-ecom-v123"): []D.InstanceID{"i-ab9a4f10"},
+							},
+							usWest2: {
+								D.ASGName("abcloud-ecom-v123"): []D.InstanceID{"i-b28a69c8"},
+							},
+							euWest1: {
+								D.ASGName("abcloud-ecom-v123"): []D.InstanceID{"i-4fa09365"},
+							},
+						},
 					},
 				},
 			},
 		},
-	}
-	return D.NewApp("abcloud", m)
+	)
 }
