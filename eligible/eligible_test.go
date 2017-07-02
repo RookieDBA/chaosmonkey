4b490a4ffed1f4889a08845d64a27d22de7d0acc
Lorin Hochstein <lhochstein@netflix.com>
2017-07-01 19:34:20 -0700

    add test

--- eligible/eligible_test.go
+++ eligible/eligible_test.go
@@ -5,7 +5,25 @@ import (
 	"github.com/Netflix/chaosmonkey/grp"
 	"github.com/Netflix/chaosmonkey/mock"
 	"testing"
+	"sort"
 )
+
+func mockDeployment() D.Deployment {
+	a := D.AccountName("prod")
+	p := "aws"
+	r := D.RegionName("us-east-1")
+
+	return &mock.Deployment{AppMap: map[string]D.AppMap{
+		"foo": {a:
+		D.AccountInfo{CloudProvider: p, Clusters:
+		D.ClusterMap{
+			"foo-prod": {r: {"foo-prod-v001": []D.InstanceID{"i-11111111", "i-22222222"}}},
+			"foo-prod-lorin": {r: {"foo-prod-lorin-v123": []D.InstanceID{"i-33333333", "i-44444444"}}},
+			"foo-staging": {r: {"foo-staging-v005": []D.InstanceID{"i-55555555", "i-66666666"}}},
+			"foo-staging-lorin": {r: {"foo-prod-lorin-v117": []D.InstanceID{"i-77777777", "i-88888888"}}},
+		}},
+		}}}
+}
 
 func TestClusterGropuing(t *testing.T) {
 	// setup
@@ -32,23 +50,6 @@ func TestClusterGropuing(t *testing.T) {
 	}
 }
 
-func mockDeployment() D.Deployment {
-	a := D.AccountName("prod")
-	p := "aws"
-	r := D.RegionName("us-east-1")
-
-	return &mock.Deployment{AppMap: map[string]D.AppMap{
-		"foo": {a:
-		D.AccountInfo{CloudProvider: p, Clusters:
-			D.ClusterMap{
-				"foo-prod": {r: {"foo-prod-v001": []D.InstanceID{"i-11111111", "i-22222222"}}},
-				"foo-prod-lorin": {r: {"foo-prod-lorin-v123": []D.InstanceID{"i-33333333", "i-44444444"}}},
-				"foo-staging": {r: {"foo-staging-v005": []D.InstanceID{"i-55555555", "i-66666666"}}},
-				"foo-staging-lorin": {r: {"foo-prod-lorin-v117": []D.InstanceID{"i-77777777", "i-88888888"}}},
-			}},
-		}}}
-}
-
 func TestStackGrouping(t *testing.T) {
 	// setup
 	dep := mockDeployment()
@@ -72,5 +73,38 @@ func TestStackGrouping(t *testing.T) {
 			t.Fatalf("got=%v, want=%v", got, want)
 		}
 	}
+}
+
+func TestAppGrouping(t *testing.T) {
+	// setup
+	dep := mockDeployment()
+	group := grp.New("foo", "prod", "us-east-1", "", "")
+
+	// code under test
+	instances, err := Instances(group, nil, dep)
+	if err != nil {
+		t.Fatalf("%+v", err)
+	}
+
+	gots := make([]string, len(instances))
+	for i, inst := range instances {
+		gots[i] = inst.ID()
+	}
+
+	sort.Strings(gots)
+
+	// assertions
+	wants := []string{"i-11111111", "i-22222222", "i-33333333", "i-44444444", "i-55555555", "i-66666666", "i-77777777", "i-88888888"}
+
+	if got, want := len(gots), len(wants); got != want {
+		t.Fatalf("len(eligible.Instances(group, cfg, app))=%v, want %v", got, want)
+	}
+
+	for i, got := range gots {
+		if want := wants[i]; got != want {
+			t.Fatalf("got=%v, want=%v", got, want)
+		}
+	}
 
 }
+
